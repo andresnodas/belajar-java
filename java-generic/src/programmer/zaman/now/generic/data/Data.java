e006022573767a65446b2f082b7172810b0123cf
Andres Nodas <nodasiniesta@gmail.com>
2022-08-20 11:15:12 +0700

    Moved from repository java-generic

---
+++ java-generic/src/programmer/zaman/now/generic/data/Data.java
@@ -0,0 +1,19 @@
+package programmer.zaman.now.generic.data;
+
+public class Data {
+
+	private Object data;
+
+	public Data(Object data) {
+		this.data = data;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+}
